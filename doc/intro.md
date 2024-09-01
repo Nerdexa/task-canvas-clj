44a782d399ff90f6f40ad03f72b462a93ae12cd4
kamegoro <tentyo0620@icloud.com>
2024-09-01 17:40:37 +0900

    update: setup

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to task-canvas-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/task_canvas_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns task-canvas-clj.core-test
+  (:require [clojure.test :refer :all]
+            [task-canvas-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
