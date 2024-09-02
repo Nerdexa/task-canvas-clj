2fbc8ac3ffa0e7a02225f4d37a55feedfa5abfc5
kamegoro <tentyo0620@icloud.com>
2024-09-02 22:42:24 +0900

    add: delete and get handler

--- src/task_canvas_clj/core.clj
+++ src/task_canvas_clj/core.clj
@@ -30,15 +30,42 @@
                           (str "Failed to post todo: " todo)
                           {:cause e}))))))
 
+(defn- delete-todo
+  [todo-id] (let [path "http://localhost:8080/todos"]
+              (try (client/delete (str path "/" todo-id))
+                   (catch Exception e
+                     (throw (ex-info
+                             (str "Failed to delete todo: " todo-id)
+                             {:cause e}))))))
+
+(defn- get-todos
+  [] (let [path "http://localhost:8080/todos"]
+       (try (client/get path)
+            (catch Exception e
+              (throw (ex-info
+                      "Failed to get todos"
+                      {:cause e}))))))
+
+(defn- get-todoIds
+  [] (let [todos (get-todos)]
+       (map :id todos)))
+
+(defn- delete-all-todos
+  [] (let [todo-ids (get-todoIds)]
+       (doseq [todo-id todo-ids]
+         (delete-todo todo-id))))
+
 (defn- create-todos
   [n] (let [todos (generate-faker-todos n)]
         (doseq [todo todos]
           (post-todo todo))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "todo-canvas-api command line interface"
   [& args]
   (let [command (first args)]
     (case command
       "ping" (system-ping)
-      "create-todos" (create-todos 20))))
+      "get-todos" (get-todos)
+      "create-todos" (create-todos 20)
+      "delete-all-todos" (delete-all-todos))))
