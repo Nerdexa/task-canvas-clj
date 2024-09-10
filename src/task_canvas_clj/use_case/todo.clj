f0b4f0483ee3d6b7756d8e715fb907d932c436f6
kamegoro <tentyo0620@icloud.com>
2024-09-10 23:38:08 +0900

    add: store todo test case

--- src/task_canvas_clj/use_case/todo.clj
+++ src/task_canvas_clj/use_case/todo.clj
@@ -1,4 +1,20 @@
-(ns task-canvas-clj.use-case.todo)
+(ns task-canvas-clj.use-case.todo
+  (:require [faker.lorem :as faker-lorem]
+            [task-canvas-clj.port.task-canvas :as task-canvas-port]))
+
+(defn- generate-fake-content
+  [n] (take n (faker-lorem/paragraphs)))
+
+(defn- generate-fake-completed
+  [] (rand-nth [true false]))
+
+(defn- generate-faker-todos
+  [n] (repeatedly n (fn [] {:content (first (generate-fake-content 1))
+                            :completed (generate-fake-completed)})))
 
 (defn store-todo
-  [{:keys [task-canvas-driver]}])
+  [{:keys [task-canvas-driver]}]
+  (let [todos (generate-faker-todos 20)]
+    (doseq [todo todos]
+      (task-canvas-port/post-todo task-canvas-driver todo))
+    nil))
--- test/task_canvas_clj/use_case/todo_test.clj
+++ test/task_canvas_clj/use_case/todo_test.clj
@@ -1,13 +1,15 @@
 (ns task-canvas-clj.use-case.todo-test
   (:require [clojure.test :as t]
-            [task-canvas-clj.use-case.todo :as sut]
-            [task-canvas-clj.port.task-canvas :as task-canvas-port]
             [mockfn.macros :as mockfn]
-            [mockfn.matchers :as matcher]))
+            [mockfn.matchers :as matcher]
+            [task-canvas-clj.port.task-canvas :as task-canvas-port]
+            [task-canvas-clj.use-case.todo :as sut]))
 
 (t/deftest store-todo-test
   (t/testing "todoの登録"
     (let [deps {:task-canvas-driver :task-canvas-driver'}
           todo {:content "content" :completed false}]
-      (mockfn/verifying [(task-canvas-port/post-todo :task-canvas-driver' todo) nil (matcher/exactly 1)]
-                        (t/is (= nil (sut/store-todo deps)))))))
+      (with-redefs [sut/generate-faker-todos (fn [_] (repeat 20 todo))]
+        (mockfn/verifying
+         [(task-canvas-port/post-todo :task-canvas-driver' todo) nil (matcher/exactly 20)]
+         (t/is (= nil (sut/store-todo deps))))))))
