000994978a5f4588be3c1934e0aa0a1e641b9315
kamegoro <tentyo0620@icloud.com>
2024-09-17 01:28:09 +0900

    add get todos driver

--- src/task_canvas_clj/driver/task_canvas.clj
+++ src/task_canvas_clj/driver/task_canvas.clj
@@ -1,11 +1,27 @@
 (ns task-canvas-clj.driver.task-canvas
-  (:require [clj-http.client :as client]))
+  (:require [clj-http.client :as client])
+  (:import [java.net URI]))
+
+(defn- url [^String path]
+  (let [base "http://localhost:8080/v1/todos"]
+    (if (empty? path)
+      base
+      (str (.resolve ^URI (URI. base) path)))))
 
 (defn post-todo
-  [todo] (let [path "http://localhost:8080/v1/todos"]
+  [todo] (let [path (url "")]
            (try (client/post path {:content-type :json
                                    :form-params todo})
                 (catch Exception e
                   (throw (ex-info
                           (str "Failed to post todo: " todo)
                           {:cause e}))))))
+
+(defn get-todos
+  [] (let [path (url "")]
+       (-> (try (client/get path)
+                (catch Exception e
+                  (throw (ex-info
+                          "Failed to get todos"
+                          {:cause e}))))
+           (get-in [:body]))))
