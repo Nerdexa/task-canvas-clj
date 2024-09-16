d4f5124d38042bfb807455f03aeec446562ab089
kamegoro <tentyo0620@icloud.com>
2024-09-17 00:46:15 +0900

    update reify driver

--- src/task_canvas_clj/core.clj
+++ src/task_canvas_clj/core.clj
@@ -1,30 +1,14 @@
 (ns task-canvas-clj.core
   (:gen-class)
-  (:require [faker.lorem :as faker-lorem]
-            [clj-http.client :as client]
-            [cheshire.core :as json]))
+  (:require
+   [clj-http.client :as client]
+   [cheshire.core :as json]
+   [task-canvas-clj.port.task-canvas :as task-canvas-port]
+   [task-canvas-clj.driver.task-canvas :as task-canvas-driver]
+   [task-canvas-clj.use-case.todo :as task-canvas-usecase]))
 
 (defn- system-ping
   [] (println "pong"))
-
-(defn- generate-fake-content
-  [n] (take n (faker-lorem/paragraphs)))
-
-(defn- generate-fake-completed
-  [] (rand-nth [true false]))
-
-(defn- generate-faker-todos
-  [n] (repeatedly n (fn [] {:content (first (generate-fake-content 1))
-                            :completed (generate-fake-completed)})))
-
-(defn- post-todo
-  [todo] (let [path "http://localhost:8080/v1/todos"]
-           (try (client/post path {:content-type :json
-                                   :form-params todo})
-                (catch Exception e
-                  (throw (ex-info
-                          (str "Failed to post todo: " todo)
-                          {:cause e}))))))
 
 (defn- delete-todo
   [todo-id] (let [path (str "http://localhost:8080/v1/todos/" todo-id)]
@@ -56,11 +40,9 @@
          (delete-todo todo-id))
        (println (str "Deleted all todos"))))
 
-(defn- create-todos
-  [n] (let [todos (generate-faker-todos n)]
-        (doseq [todo todos]
-          (post-todo todo))
-        (println (str "Created todos"))))
+(def task-canvas-driver
+  (reify task-canvas-port/TaskCanvasApiPort
+    (post-todo [_ todo] (task-canvas-driver/post-todo todo))))
 
 (defn -main
   "todo-canvas-api command line interface"
@@ -69,5 +51,5 @@
     (case command
       "ping" (system-ping)
       "get-todos" (println (get-todos))
-      "create-todos" (create-todos 20)
+      "create-todos" (task-canvas-usecase/store-todo {:task-canvas-driver task-canvas-driver})
       "delete-all-todos" (delete-all-todos))))
