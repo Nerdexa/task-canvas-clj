bf10577af77b2d18322c374ae9a7151c764b2712
kamegoro <tentyo0620@icloud.com>
2024-09-17 22:56:33 +0900

    add delete driver

--- src/task_canvas_clj/driver/task_canvas.clj
+++ src/task_canvas_clj/driver/task_canvas.clj
@@ -1,12 +1,11 @@
 (ns task-canvas-clj.driver.task-canvas
-  (:require [clj-http.client :as client])
-  (:import [java.net URI]))
+  (:require [clj-http.client :as client]))
 
 (defn- url [^String path]
   (let [base "http://localhost:8080/v1/todos"]
     (if (empty? path)
       base
-      (str (.resolve ^URI (URI. base) path)))))
+      (str base "/" path))))
 
 (defn post-todo
   [todo] (let [path (url "")]
@@ -19,9 +18,23 @@
 
 (defn get-todos
   [] (let [path (url "")]
-       (-> (try (client/get path)
-                (catch Exception e
-                  (throw (ex-info
-                          "Failed to get todos"
-                          {:cause e}))))
-           (get-in [:body]))))
+       (println "Requesting todos from URL:" path)
+       (try
+         (let [response (client/get path)]
+           (get-in response [:body]))
+         (catch Exception e
+           (println "Error during get-todos:" (.getMessage e))
+           (throw (ex-info
+                   "Failed to get todos"
+                   {:cause e}))))))
+
+(defn delete-todo [todo-id]
+  (let [path (url todo-id)]
+    (try
+      (let [response (client/delete path)]
+        (get-in response [:body]))
+      (catch Exception e
+        (println "Error during delete-todo:" (.getMessage e))
+        (throw (ex-info
+                (str "Failed to delete todo: " todo-id)
+                {:cause e}))))))
