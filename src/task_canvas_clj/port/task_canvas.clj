64e2611354c50c03e8e40ba8445225078339ff80
kamegoro <tentyo0620@icloud.com>
2024-10-08 22:09:01 +0900

    update port

--- src/task_canvas_clj/port/task_canvas.clj
+++ src/task_canvas_clj/port/task_canvas.clj
@@ -1,6 +1,7 @@
 (ns task-canvas-clj.port.task-canvas)
 
 (defprotocol TaskCanvasApiPort
-  (post-todo [this todo])
-  (get-todos [this])
-  (delete-todo [this todo-id]))
+  (post-todo [this todo authorization])
+  (get-todos [this authorization])
+  (delete-todo [this todo-id authorization])
+  (sign-in [this email password]))
