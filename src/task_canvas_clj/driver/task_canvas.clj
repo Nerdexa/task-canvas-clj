c2b92466b0719b4bf19214b00cdf786031c71317
kamegoro <tentyo0620@icloud.com>
2024-10-08 22:09:12 +0900

    update driver for authorization

--- src/task_canvas_clj/driver/task_canvas.clj
+++ src/task_canvas_clj/driver/task_canvas.clj
@@ -1,40 +1,57 @@
 (ns task-canvas-clj.driver.task-canvas
-  (:require [clj-http.client :as client]))
+  (:require [cheshire.core :as json]
+            [clj-http.client :as client]))
 
-(defn- url [^String path]
+(defn- todo-url [^String path]
   (let [base "http://localhost:8080/v1/todos"]
     (if (empty? path)
       base
       (str base "/" path))))
 
-(defn post-todo
-  [todo] (let [path (url "")]
-           (try (client/post path {:content-type :json
-                                   :form-params todo})
-                (catch Exception e
-                  (throw (ex-info
-                          (str "Failed to post todo: " todo)
-                          {:cause e}))))))
+(defn- with-auth [request authorization]
+  (assoc-in request [:headers "Authorization"] authorization))
 
-(defn get-todos
-  [] (let [path (url "")]
-       (println "Requesting todos from URL:" path)
-       (try
-         (let [response (client/get path)]
-           (get-in response [:body]))
-         (catch Exception e
-           (println "Error during get-todos:" (.getMessage e))
-           (throw (ex-info
-                   "Failed to get todos"
-                   {:cause e}))))))
-
-(defn delete-todo [todo-id]
-  (let [path (url todo-id)]
+(defn post-todo [todo token]
+  (let [path (todo-url "")]
     (try
-      (let [response (client/delete path)]
+      (client/post path (with-auth {:headers {"Content-Type" "application/json"}
+                                    :body (json/generate-string todo)} token))
+      (catch Exception e
+        (throw (ex-info
+                (str "Failed to post todo: " todo)
+                {:cause e}))))))
+
+(defn get-todos [token]
+  (let [path (todo-url "")]
+    (try
+      (let [response (client/get path (with-auth {:headers {"Content-Type" "application/json"}} token))]
+        (json/parse-string (:body response) true))
+      (catch Exception e
+        (println "Error during get-todos:" (.getMessage e))
+        (throw (ex-info
+                "Failed to get todos"
+                {:cause e}))))))
+
+(defn delete-todo [todo-id token]
+  (let [path (todo-url todo-id)]
+    (try
+      (let [response (client/delete path (with-auth {:headers {"Content-Type" "application/json"}} token))]
         (get-in response [:body]))
       (catch Exception e
         (println "Error during delete-todo:" (.getMessage e))
         (throw (ex-info
                 (str "Failed to delete todo: " todo-id)
+                {:cause e}))))))
+
+(defn sign-in [email password]
+  (let [path "http://localhost:8080/v1/signIn"]
+    (try
+      (let [response (client/post path {:headers {"Content-Type" "application/json"}
+                                        :body (json/generate-string {:email email
+                                                                     :password password})})]
+        response)
+      (catch Exception e
+        (println "Error during sign-in:" (.getMessage e))
+        (throw (ex-info
+                "Failed to sign in"
                 {:cause e}))))))
