68de1ded8a5f3343f8dd16df838b0f7d17e1e428
kamegoro <tentyo0620@icloud.com>
2024-10-08 22:08:45 +0900

    fix usecase test

--- test/task_canvas_clj/use_case/todo_test.clj
+++ test/task_canvas_clj/use_case/todo_test.clj
@@ -9,16 +9,18 @@
 (t/deftest store-todo-test
   (t/testing "todoの登録"
     (let [deps {:task-canvas-driver :task-canvas-driver'}
-          todo {:content "content" :completed false}]
+          todo {:content "content" :completed false}
+          authorization "authorization"]
       (with-redefs [sut/generate-faker-todos (fn [_] (repeat 20 todo))]
         (mockfn/verifying
-         [(task-canvas-port/post-todo :task-canvas-driver' todo) nil (matcher/exactly 20)]
-         (t/is (= nil (sut/store-todo deps))))))))
+         [(task-canvas-port/post-todo :task-canvas-driver' todo authorization) nil (matcher/exactly 20)]
+         (t/is (= nil (sut/store-todo deps authorization))))))))
 
 (t/deftest get-todos-test
   (t/testing "todosの全権取得"
     (let [deps {:task-canvas-driver :task-canvas-driver'}
-          todos {:todos [{:id "id" :content "content" :completed false}]}]
+          todos {:todos [{:id "id" :content "content" :completed false}]}
+          authorization "authorization"]
       (mockfn/verifying
-       [(task-canvas-port/get-todos :task-canvas-driver') todos (matcher/exactly 1)]
-       (t/is (match? todos (sut/get-todos deps)))))))
+       [(task-canvas-port/get-todos :task-canvas-driver' authorization) todos (matcher/exactly 1)]
+       (t/is (match? todos (sut/get-todos deps authorization)))))))
